a0ebd9e045efc87b37078b3f0e25631ff4de6da0
Salon <rautsalon@gmail.com>
2025-02-05 20:58:01 +0545

    Initial phase

---
+++ controllers/contactController.js
@@ -0,0 +1,45 @@
+//@desc Get all contacts
+//@route GET /api/contacts
+//@access public
+
+const getContacts = (req,res)=>{
+    res.status(200).json({ message: 'Get all contacts'});
+};
+
+//@desc Create new contact
+//@route POST /api/contacts
+//@access public
+
+const createContact = (req,res)=>{
+    // console.log(req.body);
+    res.status(201).json({ message: 'Create contacts'});
+};
+
+//@desc Get contact
+//@route GET /api/contacts/:id
+//@access public
+
+const getContact = (req,res)=>{
+    res.status(200).json({ message: `Get contact for ${req.params.id}`});
+};
+
+//@desc update contact
+//@route PUT /api/contacts/:id
+//@access public
+
+const updateContact = (req,res)=>{
+    res.status(200).json({ message: `Update contact for ${req.params.id}`});
+};
+
+
+//@desc delete contact
+//@route DELETE /api/contacts/:id
+//@access public
+
+const deleteContact = (req,res)=>{
+    res.status(200).json({ message: `Delete contact for ${req.params.id}`});
+};
+
+
+
+module.exports = { getContacts,getContact,createContact,updateContact,deleteContact }
